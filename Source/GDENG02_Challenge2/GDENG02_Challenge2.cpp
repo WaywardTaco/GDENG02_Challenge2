f260bc38650b85f5b6ffb8b826d9bb5f2939047b
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-11-14 09:59:21 +0800

    Initial commit

---
+++ Source/GDENG02_Challenge2/GDENG02_Challenge2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GDENG02_Challenge2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GDENG02_Challenge2, "GDENG02_Challenge2" );
